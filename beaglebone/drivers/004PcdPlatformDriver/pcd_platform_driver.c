54a9a68706eb4cbdb0b4457ff4dcdbba25b7b48e
dineshjakkam <dineshjakkam13@gmail.com>
2020-11-16 17:35:40 -0500

    Compiling platform driver

--- beaglebone/drivers/004PcdPlatformDriver/pcd_platform_driver.c
+++ beaglebone/drivers/004PcdPlatformDriver/pcd_platform_driver.c
@@ -35,7 +35,8 @@ struct pcdrv_private_data pcdrv_data;
 loff_t pcd_lseek(struct file *filep, loff_t off, int whence){
 	struct pcdev_private_data* pcdev_data = (struct pcdev_private_data *)filep->private_data;
 	
-	int max_data = pcdev_data -> size;off_t tmp;
+	int max_data = pcdev_data->pdata.size;
+	off_t tmp;
 	pr_info("lseek requested\n");
 	pr_info("Current file position = %lld\n", filep->f_pos);
 	
@@ -63,7 +64,7 @@ loff_t pcd_lseek(struct file *filep, loff_t off, int whence){
 ssize_t pcd_read(struct file *filep, char __user *buffer, size_t count, loff_t *f_pos){
 	struct pcdev_private_data* pcdev_data = (struct pcdev_private_data *)filep->private_data;
 	
-	int max_data = pcdev_data -> size;
+	int max_data = pcdev_data->pdata.size;
 	pr_info("Read requested for %zu bytes\n", count);
 	pr_info("Initial file position = %lld\n", *f_pos);
 	
@@ -89,7 +90,7 @@ ssize_t pcd_read(struct file *filep, char __user *buffer, size_t count, loff_t *
 ssize_t pcd_write(struct file *filep, const char __user *buffer, size_t count, loff_t *f_pos){
 	struct pcdev_private_data* pcdev_data = (struct pcdev_private_data *)filep->private_data;
 	
-	int max_data = pcdev_data -> size;
+	int max_data = pcdev_data->pdata.size;
 	
 	pr_info("Write requested for %zu bytes\n", count);
 	pr_info("Initial file position = %lld\n", *f_pos); 
@@ -137,7 +138,7 @@ int pcd_open(struct inode *p_inode, struct file *filep){
 
 	/* Find out which device file open was attempted by the uer space */
 	minor_n = MINOR(p_inode->i_rdev);
-	pr_info("Minore access = %d\n", minor_n);
+	pr_info("Minor access = %d\n", minor_n);
 	
 	/* Get device's private data structure */
 	pcdev_data = container_of(p_inode->i_cdev, struct pcdev_private_data, cdev);
@@ -145,9 +146,9 @@ int pcd_open(struct inode *p_inode, struct file *filep){
 	/* To supply device private data to other methods of the driver */
 	filep->private_data = pcdev_data;
 
-	pr_info("permission is %x\n", pcdev_data->perm);
+	pr_info("permission is %x\n", pcdev_data->pdata.perm);
 	/* check permissions */
-	ret = check_permission(pcdev_data->perm, filep->f_mode);
+	ret = check_permission(pcdev_data->pdata.perm, filep->f_mode);
 
 	(!ret) ? pr_info("Open was successful\n") : pr_info("Open was unsuccessfull\n");
 	
@@ -170,15 +171,32 @@ struct file_operations pcd_fops = {
 
 /* Gets called when the device is removed from the platform */
 int pcd_platform_driver_remove(struct platform_device* pdev){
-        pr_info("A device is removed\n");
+        struct pcdev_private_data* dev_data = dev_get_drvdata(&pdev->dev);
+	
+	/* Remove device that was created with device_create() */
+	device_destroy(pcdrv_data.class_pcd, dev_data->dev_num);
+
+	/* Remove cdev entry from the system */
+	cdev_del(&dev_data->cdev);
+	
+	/* Free the memory held by the device */
+	kfree(dev_data->buffer);
+	kfree(dev_data);
+
+	pcdrv_data.total_devices--;
+	
+	pr_info("A device is removed\n");
 	return 0;
 }
 
 /* Gets called when the matched platform device is found */
 int pcd_platform_driver_probe(struct platform_device* pdev){
 	
+	int ret;
 	struct pcdev_private_data *dev_data;
 	struct pcdev_platform_data *pdata;
+	
+	pr_info("A device is detected\n");
 	
 	/* 1. Get the platform data */
 	pdata = (struct pcdev_platform_data*)dev_get_platdata(&pdev->dev);
@@ -191,25 +209,65 @@ int pcd_platform_driver_probe(struct platform_device* pdev){
 	/* Dynamically allocate memory for the device private data */
 	dev_data = kzalloc(sizeof(*dev_data), GFP_KERNEL);
 	if(dev_data == NULL){
-		pr_info("cannot allocate memory\n");
+		pr_info("Cannot allocate memory for device data structure\n");
 		ret = -ENOMEM;
 		goto out;
-	}	
+	}
+	
+	dev_set_drvdata(&pdev->dev, dev_data);	
 
 	dev_data->pdata.size = pdata->size;
 	dev_data->pdata.perm = pdata->perm;
 	dev_data->pdata.serial_number = pdata->serial_number;
 
-	pr_info("Device serial number = %s\n", dev_data->serial_number);
+	pr_info("Device serial number = %s\n", dev_data->pdata.serial_number);
 	pr_info("Device size = %d\n", dev_data->pdata.size);
 	pr_info("Device permission = %d\n", dev_data->pdata.perm);
 
-	/* Dynamically allocate memory for the devuce buffer using size
+	/* Dynamically allocate memory for the device buffer using size
 	information from the platform data */
+	dev_data->buffer = kzalloc(sizeof(dev_data->pdata.size), GFP_KERNEL);
+        if(dev_data->buffer == NULL){
+                pr_info("Cannot allocate memory for device buffer\n");
+                ret = -ENOMEM;
+                goto dev_data_free;
+        }
+
+	/* Get the device number */
+	dev_data->dev_num = pcdrv_data.device_num_base + pdev->id;
+
+	/* Do cdev init and cdev add */
+	cdev_init(&dev_data->cdev, &pcd_fops);
+
+	dev_data->cdev.owner = THIS_MODULE;
+	ret = cdev_add(&dev_data->cdev, dev_data->dev_num, 1);
+	if(ret < 0 ){
+		pr_err("Cdev add failed\n");
+		goto buffer_free;
+	}
+
+	/* Create device file for the detected platform device */
+	pcdrv_data.device_pcd = device_create(pcdrv_data.class_pcd, NULL, dev_data->dev_num, NULL, "pcdev-%d", pdev->id);
+	if(IS_ERR(pcdrv_data.device_pcd)){
+		pr_err("Device create failed \n");
+		ret = PTR_ERR(pcdrv_data.device_pcd);
+		goto cdev_del;
+	}
+
+	pcdrv_data.total_devices++;
 	
-	
-	pr_info("A device is detected\n");
+	pr_info("The probe is successful\n");
 	return 0;
+
+cdev_del:
+	cdev_del(&dev_data->cdev);;
+buffer_free:
+	kfree(dev_data->buffer);
+dev_data_free:
+	kfree(dev_data);
+out:
+	pr_info("Device probe failed\n");
+	return ret;
 }
 
 struct platform_driver pcd_platform_driver = {
